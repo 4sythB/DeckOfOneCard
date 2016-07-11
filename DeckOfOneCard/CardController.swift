928b7b59e5ac4e82400fc6ee4ce68b0d4b3f5424
Brad Forsyth <b4syth@gmail.com>
2016-07-11 12:34:37 -0600

    Added an Image Controller, implemented in the View Controller, and fixed constraints.

--- DeckOfOneCard/CardController.swift
+++ DeckOfOneCard/CardController.swift
@@ -13,7 +13,26 @@ class CardController {
     static let baseURL = NSURL(string: "http://deckofcardsapi.com/api/deck/new/draw/")
     
     static func drawCards(numberOfCards: Int, completion: ((card: [Card]) -> Void )) {
+        guard let url = baseURL else { fatalError("URL optional is nil") }
         
-        
+        let urlParameters = ["count" : "\(numberOfCards)"]
+        NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
+            if let data = data,
+                responseDataString = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                
+                guard let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: [])) as? [String : AnyObject],
+                    cardsArray = jsonDictionary["cards"] as? [[String : AnyObject]] else {
+                        print("Unable to serialize JSON. \n\(responseDataString)")
+                        completion(card: [])
+                        return
+                }
+                let cards = cardsArray.flatMap { Card(dictionary: $0) }
+                completion(card: cards)
+                
+            } else {
+                print("No Data returned from network request")
+                completion(card: [])
+            }
+        }
     }
 }
--- DeckOfOneCard/CardViewController.swift
+++ DeckOfOneCard/CardViewController.swift
@@ -11,33 +11,16 @@ import UIKit
 class CardViewController: UIViewController {
     
     @IBOutlet weak var cardImageView: UIImageView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     // MARK: - Action Button
     
     @IBAction func drawButtonTapped(sender: AnyObject) {
-        
-        
+        CardController.drawCards(1) { (card) in
+            guard let card = card.first else { return }
+            ImageController.imageForURL(card.imageURL, completion: { (image) in
+                guard let image = image else { return }
+                self.cardImageView.image = image
+            })
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
